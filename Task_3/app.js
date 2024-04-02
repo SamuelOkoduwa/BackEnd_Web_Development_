75ec9c669fede154c7c3b98f5e4df7bf790e37a5
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-04-02 15:44:13 +0100

    JavaScript functions

---
+++ Task_3/app.js
@@ -0,0 +1,14 @@
+// Function to calculate the area
+function calculateArea(length, width) {
+    return length * width;
+}
+
+// Function for reverse string
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// IsEven Function
+function isEven(number) {
+    return number % 2 === 0;
+}
